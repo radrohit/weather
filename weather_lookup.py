6f8c0ce5022d5ab0adc3ec67b6df7b65a9db75c9
radrohit <radrohit@gmail.com>
2015-04-15 13:20:35 -0400

    Added the URL for api request
    Weather underground api requires requests to be made through web
    requests

--- weather_lookup.py
+++ weather_lookup.py
@@ -2,4 +2,23 @@ import urllib2
 import json
 
 
-zip_code = input('Enter zip code')
+## converts a list to string
+def convert_to_urlstring(element_list):
+	string_url = ''
+	for e in element_list:
+		if isinstance(e,str):
+			string_url += e
+		else:
+			string_url += str(e)
+		string_url += '/'
+	return string_url[:-1] # we don't want / at the end of the string
+
+zip_code = input('Enter zip code: ')
+query = [zip_code]
+api_key = '2d70303b01f33a3c'
+feature = ['forecast'] 
+
+# store the url in url_string
+url_string = 'http://api.wunderground.com/api/' + api_key + '/' + convert_to_urlstring(feature) + '/q/' + convert_to_urlstring(query) +'.json'
+
+print url_string
